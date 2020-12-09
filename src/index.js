f045dc395de6ea481cb7696f1ac8022721559d60
kamalakar <kg@gmail.com>
2020-12-10 02:29:58 +0530

    checked for max request qithin given time

--- src/index.js
+++ src/index.js
@@ -1,37 +1,50 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
-let posts=require('./initialData');
+let posts = require("./initialData");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
-let max=10;
-app.get('/api/posts',(req,res)=>{
-   
- if(Number(req.query.max)<=20)
-    {
-        
-        res.send(posts.slice(0,Number(req.query.max)));
+let max = 10;
+prevMax=null;
+let requestCount = 0;
+let timeOutId = null;
+const setInitials = () => {
+  prevMax = null;
+  requestCount = 0;
+  timeOutId = null;
+};
+app.get("/api/posts", (req, res) => {
+  requestCount++;
+  noOfPosts=Number(req.query.max);
+  if (requestCount <= 5) {
+    if (noOfPosts <= 20) {
+        if(prevMax)
+        {
+            noOfPosts=noOfPosts<prevMax?noOfPosts:prevMax;
+        }
+        prevMax=noOfPosts;//>prevMax?noOfPosts:prevMax;
+
+      res.send(posts.slice(0, noOfPosts));
+    } else {
+      res.send(posts.slice(0, max));
     }
-    else
-    {
-        res.send(posts.slice(0,max));
-    }
-  
- /* setTimeout(()=>{
-    setInterval(,1)
+  } else {
+      res.status(429).send({message: "Exceed Number of API Calls"});
 
-  },0);*/
+  }
 
-})
+  if (timeOutId == null) {
+    timeOutId = setTimeout(setInitials, 30 * 1000);
+  }
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
