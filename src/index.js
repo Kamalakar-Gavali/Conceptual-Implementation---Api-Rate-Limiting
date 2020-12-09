e5cacbffac00f6df717ff291a2e1c4c8c9eac3f1
kamalakar <kg@gmail.com>
2020-12-10 01:48:32 +0530

    partially done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
 app.use(express.urlencoded());
-
+let posts=require('./initialData');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
@@ -12,7 +12,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+let max=10;
+app.get('/api/posts',(req,res)=>{
+   
+ if(Number(req.query.max)<=20)
+    {
+        
+        res.send(posts.slice(0,Number(req.query.max)));
+    }
+    else
+    {
+        res.send(posts.slice(0,max));
+    }
+  
+ /* setTimeout(()=>{
+    setInterval(,1)
 
+  },0);*/
+
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
